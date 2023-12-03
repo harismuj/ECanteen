c8ce3fe47ce217e599ba938bfe0a05451d8c55b3
Haris Mujianto <harismujianto5@gmail.com>
2023-12-04 02:34:15 +0700

    logo, icon, splashscreen

---
+++ app/src/main/java/com/jungkatjungkit/ecanteen/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.jungkatjungkit.ecanteen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 3000; // Waktu tampilan splash screen dalam milidetik (3 detik)
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
